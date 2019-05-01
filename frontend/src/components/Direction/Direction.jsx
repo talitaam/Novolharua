fac6fc4a9dbc8e1b5930851b6d3837fadf3bd32d
João Américo Caiafa <joaocaiafa@outlook.com>
2019-05-01 20:23:32 -0300

    layout correct

--- frontend/src/components/Direction/Direction.jsx
+++ frontend/src/components/Direction/Direction.jsx
@@ -1,11 +1,5 @@
 import React from "react";
-import {
-  compose,
-  withProps,
-  withState,
-  withHandlers,
-  mapProps
-} from "recompose";
+import { compose, withProps, withState, withHandlers, mapProps } from "recompose";
 import {
   withScriptjs,
   withGoogleMap,
@@ -17,95 +11,80 @@ import {
 const MAX_ROUTE_POINTS = 10;
 
 const enhance = compose(
-  withProps({
-    googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyAXRoo8kh7I1XuGA_OjinkIoZ4mEJ0w4j4&v=3.exp&libraries=geometry,drawing,places",
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
-    mapElement: <div style={{ height: `100%` }} />
-  }),
-  withState("waypoints", "setWaypoints", []),
-  withState("directions", "setDirection", []),
-  withHandlers({
-    clickHandler: props => event => {
-      const { waypoints, setWaypoints } = props;
-      if (waypoints.length > MAX_ROUTE_POINTS) {
-        alert(
-          "Limite de pontos máximo atigido ! Não é possível adicionar mais pontos !"
-        );
-      } else {
-        waypoints.push({
-          location: event.latLng,
-          stopover: true
-        });
-        setWaypoints(() => waypoints);
-      }
-    },
-    rightClickHandler: props => () => {
-      const { google } = window;
-      const { waypoints, setDirection, setWaypoints, travelMode } = props;
+    withProps({
+      googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAXRoo8kh7I1XuGA_OjinkIoZ4mEJ0w4j4&v=3.exp&libraries=geometry,drawing,places",
+      loadingElement: <div style={{ height: `100%` }} />,
+      containerElement: <div style={{ height: `400px` }} />,
+      mapElement: <div style={{ height: `100%` }} />
+    }),
+    withState('waypoints', 'setWaypoints', []),
+    withState('directions', 'setDirection', []),
+    withHandlers({
+      clickHandler: props => event => {
+        const { waypoints, setWaypoints } = props;
+        if (waypoints.length > MAX_ROUTE_POINTS) {
+          alert('Limite de pontos máximo atigido ! Não é possível adicionar mais pontos !');
+        } else {
+          waypoints.push({
+            location: event.latLng,
+            stopover: true
+          });
+          setWaypoints(() => waypoints);
+        }
+      },
+      rightClickHandler: props => () => {
+        const { google } = window;
+        const { waypoints, setDirection, setWaypoints, travelMode } = props;
 
-      if (!waypoints.length || waypoints.length < 2) {
-        alert(
-          "Pontos insuficientes para calcular uma rota ! É preciso de no mínimo 3 !"
-        );
-      } else {
-        const waypointsAux = waypoints.slice(0);
-        const origin = waypointsAux.shift().location;
-        const destination = waypointsAux.pop().location;
+        if (!waypoints.length || waypoints.length < 2) {
+          alert('Pontos insuficientes para calcular uma rota ! É preciso de no mínimo 3 !');
+        } else {
+          const waypointsAux = waypoints.slice(0);
+          const origin = waypointsAux.shift().location;
+          const destination = waypointsAux.pop().location;
 
-        const DirectionsService = new google.maps.DirectionsService();
+          const DirectionsService = new google.maps.DirectionsService();
 
-        DirectionsService.route(
-          {
+          DirectionsService.route({
             origin: origin,
             destination: destination,
             waypoints: waypointsAux,
-            travelMode: google.maps.TravelMode["WALKING"]
-          },
-          (result, status) => {
+            travelMode: google.maps.TravelMode['WALKING'],
+          }, (result, status) => {
             if (status === google.maps.DirectionsStatus.OK) {
+              console.log(result);
               setDirection(() => result);
               setWaypoints(() => []);
             } else {
               console.error(`Erro ao buscar rota : ${result}`);
             }
-          }
-        );
+          });
+        }
       }
-    }
-  }),
-  mapProps(({ waypoints, ...rest }) => {
-    return {
-      markers: waypoints.map((point, index) => {
-        return <Marker position={point.location} key={index} />;
-      }),
-      ...rest
-    };
-  }),
-  withScriptjs,
-  withGoogleMap
+    }),
+    mapProps(({ waypoints, ...rest }) => {
+      return (
+          {
+            markers: waypoints.map((point, index) => {
+              return (<Marker position={point.location} key={ index } />);
+            }),
+            ...rest
+          }
+      )
+    }),
+    withScriptjs,
+    withGoogleMap
 );
 
-const Direction = enhance(
-  ({
-    clickHandler,
-    rightClickHandler,
-    markers,
-    directions,
-    defaultZoom,
-    defaultCenter
-  }) => (
+const Direction = enhance(({ clickHandler, rightClickHandler, markers, directions, defaultZoom, defaultCenter }) => (
     <GoogleMap
-      defaultZoom={14}
-      defaultCenter={new window.google.maps.LatLng(-19.932654, -43.93602)}
-      onClick={clickHandler}
-      onRightClick={rightClickHandler}
-    >
-      {markers}
-      {directions && <DirectionsRenderer directions={directions} />}
-    </GoogleMap>
-  )
+        defaultZoom={ 14 }
+        defaultCenter={ new window.google.maps.LatLng(-19.932654, -43.936020) }
+        onClick={ clickHandler }
+        onRightClick={ rightClickHandler } >
+      { markers }
+      { directions && <DirectionsRenderer directions={directions} /> }
+    </GoogleMap>)
 );
 
 export default Direction;
--- frontend/src/screens/Maps/CadRotas.jsx
+++ frontend/src/screens/Maps/CadRotas.jsx
@@ -1,210 +1,223 @@
 import React from "react";
 
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+
 import Direction from "components/Direction/Direction.jsx";
 import GridItem from "components/Grid/GridItem.jsx";
 import GridContainer from "components/Grid/GridContainer.jsx";
 import Select from "components/Select/Select.jsx";
 import Button from "components/CustomButtons/Button.jsx";
 import CustomInput from "../../components/CustomInput/CustomInput";
+import Map from "components/Map/Map.jsx";
 
-const modesOfTraveling = [
+import moment from "moment";
+
+const styles = {
+  cardCategoryWhite: {
+    color: "rgba(255,255,255,.62)",
+    margin: "0",
+    fontSize: "14px",
+    marginTop: "0",
+    marginBottom: "0"
+  },
+  cardTitleWhite: {
+    color: "#FFFFFF",
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: "3px",
+    textDecoration: "none"
+  }
+};
+
+const mapsPlaces = [
   {
-    label: "Driving",
-    value: "DRIVING"
+    id: "praca_liberdade",
+    defaultBounds: {
+      north: -19.929512,
+      south: -19.933168,
+      east: -43.933712,
+      west: -43.940968
+    },
+    defaultUrl: "https://i.ibb.co/xmL7qjT/Rota-Pra-a-da-Liberdade.jpg",
+    defaultZoom: 17.3,
+    defaultCenter: { lat: -19.93134, lng: -43.93734 }
   },
   {
-    label: "Walking",
-    value: "WALKING"
+    id: "praca_savassi",
+    defaultZoom: 17.3,
+    defaultBounds: {
+      north: -19.936686,
+      south: -19.939718,
+      east: -43.933951,
+      west: -43.939428
+    },
+    defaultCenter: { lat: -19.938202, lng: -43.9366895 },
+    defaultUrl: "https://i.ibb.co/FgRtynK/Rota-Praca-Savassi.jpg"
   },
   {
-    label: "Bicycling",
-    value: "BICYCLING"
-  },
-  {
-    label: "Transit",
-    value: "TRANSIT"
+    id: "area_hospitalar",
+    defaultZoom: 17.3,
+    defaultBounds: {
+      north: -19.922602,
+      south: -19.926201,
+      east: -43.923093,
+      west: -43.929062
+    },
+    defaultCenter: { lat: -19.9244015, lng: -43.9260775 },
+    defaultUrl: "https://i.ibb.co/F4GzSn5/Rota-Area-Hospitalar.jpg"
   }
 ];
 
-const CadRotas = () => (
-  <div>
-    <GridContainer justify="center" alignItems="baseline">
-      <GridItem xs={12} sm={12} md={8}>
-        <CustomInput
-          labelText="Nome da rota:"
-          id="float"
-          formControlProps={{
-            fullWidth: true
-          }}
-        />
-        <CustomInput
-          labelText="Número de pessoas atendidas:"
-          id="float"
-          formControlProps={{
-            fullWidth: true
-          }}
-        />
-      </GridItem>
-      <GridItem xs={12} sm={12} md={4}>
-        <Select
-          options={modesOfTraveling}
-          value={modesOfTraveling[0]}
-          onChange={() => {}}
-          placeholder={"Selecione :"}
-          noOptionsMessage={"Não há rotas disponíveis !"}
-        />
-        <Button color="danger">Limpar Marcadores</Button>
-        <Button color="success">Salvar</Button>
-      </GridItem>
-    </GridContainer>
-    <GridContainer>
-      <GridItem xs={12} sm={12} md={12}>
-        <Direction />
-      </GridItem>
-    </GridContainer>
-  </div>
-);
+class CadRotas extends React.Component {
+  constructor() {
+    super();
+    this.message = "Rota agendada com sucesso!";
 
-export default CadRotas;
-
-        this.cadastrarRota = this.cadastrarRota.bind(this);
-        this.buscarRotas = this.buscarRotas.bind(this);
-        this.changeRoute = this.changeRoute.bind(this);
-        this.renderMap = this.renderMap.bind(this);
-    }
-
-    componentDidMount() {
-        // this.buscarRotas();
-    }
-
-    fetchRotas(date) {
-        date = date || moment();
-
-        return fetch('http://localhost:3001/rota/', {
-            method: "POST",
-            body: JSON.stringify({ 'data': moment(date).format('YYYY-MM-DD') })
-        }).then((res) => res.json());
-    }
-
-    buscarRotas() {
-        this.fetchRotas()
-            .then(json => {
-                const rotas = Object.values(json.rotas);
-                this.setState({
-                    rotas: rotas,
-                    rota: rotas.length > 0 ? rotas[0] : "",
-                    map: rotas.filter(map => map.id === rotas[0].id)
-                });
-            });
-    }
-
-    cadastrarRota() {
-        const doacao = {
-            rota: this.state.rota.id
-        };
-        let canSave = true;
-
-        if (!doacao.rota) {
-            alert("É preciso que uma rota seja selecionada!");
-            canSave = false;
-        }
-
-        if (canSave) {
-            fetch('http://localhost:3001/doacao/add',
-                {
-                    method: "POST",
-                    body: JSON.stringify(doacao)
-                }
-            ).then(res => res.json())
-                .then(json => {
-                    console.log(json);
-                    alert(json.message);
-                }).catch(error => {
-                    alert("Erro ao enviar cadastro de rota");
-                });
-        }
+    this.state = {
+      rota: "",
+      rotas: [],
+      map: []
     };
 
-    changeRoute(rota) {
-        const maps = mapsPlaces.filter(map => map.id === rota.value);
-        this.setState({
-            map: maps,
-            rota: rota
+    this.cadastrarRota = this.cadastrarRota.bind(this);
+    this.buscarRotas = this.buscarRotas.bind(this);
+    this.changeRoute = this.changeRoute.bind(this);
+    this.renderMap = this.renderMap.bind(this);
+  }
+
+  componentDidMount() {
+    // this.buscarRotas();
+  }
+
+  fetchRotas(date) {
+    date = date || moment();
+
+    return fetch("http://localhost:3001/rota/", {
+      method: "POST",
+      body: JSON.stringify({ data: moment(date).format("YYYY-MM-DD") })
+    }).then(res => res.json());
+  }
+
+  buscarRotas() {
+    this.fetchRotas().then(json => {
+      const rotas = Object.values(json.rotas);
+      this.setState({
+        rotas: rotas,
+        rota: rotas.length > 0 ? rotas[0] : "",
+        map: rotas.filter(map => map.id === rotas[0].id)
+      });
+    });
+  }
+
+  cadastrarRota() {
+    const doacao = {
+      rota: this.state.rota.id
+    };
+    let canSave = true;
+
+    if (!doacao.rota) {
+      alert("É preciso que uma rota seja selecionada!");
+      canSave = false;
+    }
+
+    if (canSave) {
+      fetch("http://localhost:3001/doacao/add", {
+        method: "POST",
+        body: JSON.stringify(doacao)
+      })
+        .then(res => res.json())
+        .then(json => {
+          console.log(json);
+          alert(json.message);
+        })
+        .catch(error => {
+          alert("Erro ao enviar cadastro de rota");
         });
     }
+  }
 
-    renderMap(mapData) {
-        return (
-            <Map
-                key={mapData.id}
-                defaultBounds={mapData.defaultBounds}
-                defaultUrl={mapData.defaultUrl}
-                defaultZoom={mapData.defaultZoom}
-                defaultCenter={mapData.defaultCenter}
+  changeRoute(rota) {
+    const maps = mapsPlaces.filter(map => map.id === rota.value);
+    this.setState({
+      map: maps,
+      rota: rota
+    });
+  }
+
+  renderMap(mapData) {
+    return (
+      <Map
+        key={mapData.id}
+        defaultBounds={mapData.defaultBounds}
+        defaultUrl={mapData.defaultUrl}
+        defaultZoom={mapData.defaultZoom}
+        defaultCenter={mapData.defaultCenter}
+      />
+    );
+  }
+
+  render() {
+    const modesOfTraveling = [
+      {
+        label: "Driving",
+        value: "DRIVING"
+      },
+      {
+        label: "Walking",
+        value: "WALKING"
+      },
+      {
+        label: "Bicycling",
+        value: "BICYCLING"
+      },
+      {
+        label: "Transit",
+        value: "TRANSIT"
+      }
+    ];
+
+    return (
+      <div>
+        <GridContainer justify="center" alignItems="baseline">
+          <GridItem xs={12} sm={12} md={8}>
+            <CustomInput
+              labelText="Nome da rota:"
+              id="float"
+              formControlProps={{
+                fullWidth: true
+              }}
             />
-        );
-    }
-
-    render() {
-        const modesOfTraveling = [
-            {
-                label: "Driving",
-                value: "DRIVING"
-            },
-            {
-                label: "Walking",
-                value: "WALKING"
-            },
-            {
-                label: "Bicycling",
-                value: "BICYCLING"
-            },
-            {
-                label: "Transit",
-                value: "TRANSIT"
-            }
-        ];
-        
-        return (
-            <div>
-    <GridContainer justify="center" alignItems="baseline">
-      <GridItem xs={12} sm={12} md={8}>
-        <CustomInput
-          labelText="Nome da rota:"
-          id="float"
-          formControlProps={{
-            fullWidth: true
-          }}
-        />
-        <CustomInput
-          labelText="Número de pessoas atendidas:"
-          id="float"
-          formControlProps={{
-            fullWidth: true
-          }}
-        />
-      </GridItem>
-      <GridItem xs={12} sm={12} md={4}>
-        <Select
-          options={modesOfTraveling}
-          value={modesOfTraveling[0]}
-          onChange={() => {}}
-          placeholder={"Selecione :"}
-          noOptionsMessage={"Não há rotas disponíveis !"}
-        />
-        <Button color="danger">Limpar Marcadores</Button>
-        <Button color="success">Salvar</Button>
-      </GridItem>
-    </GridContainer>
-    <GridContainer>
-      <GridItem xs={12} sm={12} md={12}>
-        <Direction />
-      </GridItem>
-    </GridContainer>
-  </div>
-        );
-    }
+            <CustomInput
+              labelText="Número de pessoas atendidas:"
+              id="float"
+              formControlProps={{
+                fullWidth: true
+              }}
+            />
+          </GridItem>
+          <GridItem xs={12} sm={12} md={4}>
+            <Select
+              options={modesOfTraveling}
+              value={modesOfTraveling[0]}
+              onChange={() => {}}
+              placeholder={"Selecione :"}
+              noOptionsMessage={"Não há rotas disponíveis !"}
+            />
+            <Button color="danger">Limpar Marcadores</Button>
+            <Button color="success">Salvar</Button>
+          </GridItem>
+        </GridContainer>
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={12}>
+            <Direction />
+          </GridItem>
+        </GridContainer>
+      </div>
+    );
+  }
 }
 
-
-export default (withStyles(styles)(CadRotas));
+export default withStyles(styles)(CadRotas);
